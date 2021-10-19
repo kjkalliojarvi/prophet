f7a4840e124def501deee4f0e834b3b736634817
Kari Kalliojärvi <kari.kalliojarvi@elisa.fi>
2021-10-19 15:10:00 +0300

    Modify version number

--- python/setup.py
+++ python/setup.py
@@ -305,7 +305,7 @@ with open("requirements.txt", "r") as f:
 
 setup(
     name="prophet",
-    version="1.0.1-nopystan",
+    version="1.0.1nopystan",
     description="Automatic Forecasting Procedure",
     url="https://facebook.github.io/prophet/",
     author="Sean J. Taylor <sjtz@pm.me>, Ben Letham <bletham@fb.com>",
